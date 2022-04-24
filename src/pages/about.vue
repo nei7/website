faa4b618ada9425688bbf068e1d325f7eba44bac
nei7 <franciszek.szarek2@gmail.com>
2022-04-24 14:27:50 +0200

    chore: update about page

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -4,18 +4,14 @@
         <div class="text-lg md:text-xl tracking-wide leading-relaxed font-medium text-gray-300">
             <p
                 class="mb-4"
-            >I love spending time on learning new things. I have pretty decent experience on building stuff that lives on the internet</p>
-
+            >I love spending time on learning new things. I have pretty decent experience on building stuff that lives on the internet.</p>
             <p class="mb-4">
                 I was developing one of the biggest platforms
                 <span
                     class="text-gray-400 italic"
-                >(hundreds of thousands unique users monthly)</span> that helps students cheat on their online exams/assigments
+                >(hundreds of thousands unique users monthly)</span> that helps students cheats on their online exams/assigments
             </p>
-            <p>
-                Besides that I'm attending to high school with IT specialization in Poland, which is one of my
-                bad decisions cause outdated and poor curriculum and low commitment from my vocational teachers
-            </p>
+            <p>Besides that I'm attending to high school with computer science specialization in Poland.</p>
         </div>
     </div>
 </template>
